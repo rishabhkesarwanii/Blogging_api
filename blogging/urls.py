d51dbd06692e78b69d4980ab7a04a54fba263d55
Rishabh Kesarwani <rkesari2002@gmail.com>
2023-05-08 03:28:38 +0530

    Edited URL

--- blogging/urls.py
+++ blogging/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("api.urls",  'api'), namespace="api"), #include the api urls
+    path('api/', include(("api.urls",  'api'), namespace="api")), #include the api urls
 ]
 
 if settings.DEBUG:
